a845667e58c6d6d4e1a40beeb043aec7964abfdd
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-16 20:21:49 +0530

    Added Skeleton to New Issue page

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,19 @@
+import { Box, Card, Flex, Heading } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssueLoadingPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <form className="space-y-3">
+        <Skeleton />
+        <Skeleton height={"20rem"} />
+
+        <Skeleton />
+      </form>
+    </Box>
+  );
 };
 
 export default NewIssueLoadingPage;
