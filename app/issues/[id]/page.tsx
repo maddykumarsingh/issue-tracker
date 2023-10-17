562728da3f0d4d51e808fb649bdd00164d067542
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-17 09:55:33 +0530

    Refactor Issue Detail  Page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 import Markdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetail from "./IssueDetail";
 
 interface Props {
   params: { id: string };
@@ -21,20 +23,10 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="3" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()} </Text>
-        </Flex>
-        <Card className="prose">
-          <Markdown>{issue.description}</Markdown>
-        </Card>
+        <IssueDetail issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon></Pencil2Icon>
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
