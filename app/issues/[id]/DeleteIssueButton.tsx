e6fb25ec5e9156c61cf5b8c24fa1af769e35e8b1
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-17 14:50:45 +0530

    Show a Dialogue Confirmation Box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,14 +1,37 @@
+"use client";
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <TrashIcon></TrashIcon>
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <>
+            <TrashIcon></TrashIcon>
+            Delete Issue
+          </>
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Delete ?.</AlertDialog.Title>
+        <AlertDialog.Description>
+          {" "}
+          Are you sure you want to delete the Issue ? Action are not undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="4">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
