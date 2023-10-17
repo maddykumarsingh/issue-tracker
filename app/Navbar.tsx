51411e4ec8d15f4584fe15c8e715ccb0ad96980d
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-17 22:39:02 +0530

    Added Skeleton to Login Link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,18 +4,75 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
+import { Skeleton } from "@/app/components";
 
-const Navbar = () => {
+const Menus = () => {
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
   const menus = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
   ];
+  return (
+    <ul className="flex space-x-6">
+      {menus.map((menu) => (
+        <li key={menu.href}>
+          <Link
+            className={classNames({
+              "text-zinc-950": menu.href === currentPath,
+              "text-zinc-500": menu.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={menu.href}
+          >
+            {menu.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
+const AuthMenu = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return <Skeleton width="3rem" />;
+  if (status === "unauthenticated")
+    return <Link href={"/api/auth/signin"}>Log In</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            radius="full"
+            size="3"
+          ></Avatar>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text>{session!.user!.email} </Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const Navbar = () => {
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -24,31 +81,9 @@ const Navbar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6">
-              {menus.map((menu) => (
-                <li key={menu.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-950": menu.href === currentPath,
-                      "text-zinc-500": menu.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={menu.href}
-                  >
-                    {menu.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <Menus />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <Link href={"/api/auth/signout"}>Logout</Link>
-            )}
-            {status === "unauthenticated" && (
-              <Link href={"/api/auth/signin"}>Log In</Link>
-            )}
-          </Box>
+          <AuthMenu></AuthMenu>
         </Flex>
       </Container>
     </nav>
