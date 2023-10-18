4c146c320f3b4d2029185cf08499219662c223df
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-18 15:10:27 +0530

    Sorting Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -27,7 +27,12 @@ const IssuePage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
-  const issues = await prisma.issue.findMany({ where: { status } });
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+  const issues = await prisma.issue.findMany({ where: { status }, orderBy });
 
   return (
     <div>
