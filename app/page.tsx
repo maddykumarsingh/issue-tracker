b8bfae78a5ee27575e00f4c3559a5ac40239dbdb
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-18 16:28:04 +0530

    Build the Layout of Pagination.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@radix-ui/themes";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <div>Dashboard</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
 }
