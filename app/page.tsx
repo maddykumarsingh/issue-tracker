fc88e6cf4c9b74beb0010ee58f0d23d40861c907
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2023-10-18 17:53:36 +0530

    Builded Issue Summary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <LatestIssues></LatestIssues>;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  return (
+    <Fragment>
+      {/* <LatestIssues></LatestIssues>
+      <IssueSummary
+        open={open}
+        inProgress={inProgress}
+        closed={closed}
+      ></IssueSummary> */}
+    </Fragment>
+  );
 }
